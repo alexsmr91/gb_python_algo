2a23bf747a6eb958b0d3e9e2b66123d458c00a0e
alex <alex_smr_91@mail.ru>
2022-05-17 15:22:55 +0400

    дз 2 по алгоритмам

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -17,3 +17,24 @@
 Введите число: 123
 Количество четных и нечетных цифр в числе равно: (1, 2)
 """
+
+
+def digits(num: int):
+    if num == 0:
+        return 0, 0
+    odd = 0
+    even = 0
+    last = num % 10
+    head = num // 10
+    if last % 2 == 1:
+        odd = 1
+    elif last != 0:
+        even = 1
+    sub_res = digits(head)
+    return sub_res[0] + even, sub_res[1] + odd
+
+
+if __name__ == "__main__":
+    tst = [123, 122, 111, 222, 1000, 0, 2000]
+    for ts in tst:
+        print(f"Количество четных и нечетных цифр в числе {ts} равно: {digits(ts)}")
